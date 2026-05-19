19fb0c0499aa732e051b42646f1dc2254ba0d73f
sasanBaho <baho.sasan@gmail.com>
2026-05-19 19:42:13 -0400

    clean: remove node_modules, .next cache, nested folder; rename to localibo-app

--- yardyman-app/yardyman-app/.next/dev/static/chunks/yardyman-app_yardyman-app_app_favicon_ico_mjs_37983988._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/c4fbe_next_dist_a312caf3._.js"
-],
-    source: "dynamic"
-});
